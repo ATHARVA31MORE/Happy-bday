97abc41726f6903632afb592a962e062a4f58566
ATHARVA31MORE <146457103+ATHARVA31MORE@users.noreply.github.com>
2024-07-03 17:48:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function displayMessage() {
+    const surpriseMessage = document.getElementById('surprise-message');
+    surpriseMessage.textContent = "You are amazing! Enjoy your special day!";
+    surpriseMessage.style.display = 'block';
+}
